07457d5dadd12a7138a6a121d8ada39702c7659d
ludiatrix <riza@nysom.com>
2026-06-16 12:05:21 -0700

    The GameController.cs now uses PhaseObjects to go to phases.

--- Assets/_Code/GameController.cs
+++ Assets/_Code/GameController.cs
@@ -1,14 +1,52 @@
+using System;
+using System.Linq;
+using LSG.Phases;
 using UnityEngine;
 
 namespace LSG
 {
+    /// <summary>
+    /// Primary Controller with the ability to go to different phases of the game. It primarily listens to commands from GameEvents.cs unless explicitly told otherwise.
+    /// </summary>
     public class GameController : MonoBehaviour
     {
+        public PhaseObject[] PhaseObjects;
         public Enums.GameState CurrentPhase = Enums.GameState.StartPhase;
+
+        private void OnEnable()
+        {
+            GameEvents.StartGame?.AddListener(GoToPhase);
+        }
+
+        /// <summary>
+        /// Goes to another phase. You can go to previous phases as well! Just not the same one.
+        /// </summary>
+        /// <param name="targetPhase">the phase you want to go to</param>
+        public void GoToPhase(Enums.GameState targetPhase)
+        {
+            if (Equals(targetPhase, CurrentPhase))
+                return;
+
+            TryEndPhase(CurrentPhase);
+            TryStartPhase(targetPhase);
+            CurrentPhase = targetPhase;
+        }
+
+        private void TryEndPhase(Enums.GameState phaseToEnd)
+        {
+            PhaseObject phaseObject = GetPhaseObjectFromGameState(phaseToEnd);
+            phaseObject.GameObject.GetComponent<Phase>().EndPhase();
+        }
         
-        [Header("Developer Mode")] 
-        public bool previousState = false;
-        public bool nextState = false;
+        private void TryStartPhase(Enums.GameState phaseToStart)
+        {
+            PhaseObject phaseObject = GetPhaseObjectFromGameState(phaseToStart);
+            phaseObject.GameObject.GetComponent<Phase>().StartPhase();
+        }
+
+        private PhaseObject GetPhaseObjectFromGameState(Enums.GameState gameState)
+        {
+            return PhaseObjects.FirstOrDefault(pObject => pObject.Phase == gameState);
+        }
     }
-    
 }
