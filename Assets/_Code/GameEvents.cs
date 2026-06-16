4f410c3c309512005d0727f304e20b5a3fe189ca
ludiatrix <riza@nysom.com>
2026-06-16 12:04:32 -0700

    Add an extention for UnityEvents to take GameStates

--- Assets/_Code/GameEvents.cs
+++ Assets/_Code/GameEvents.cs
@@ -2,8 +2,14 @@ using UnityEngine.Events;
 
 namespace LSG
 {
+    /// <summary>
+    /// This static class is the primary communicator and bus for our observer pattern. All our events and actions should probably go through this so it's easier to find bottlenecks and issues :3
+    /// </summary>
     public static class GameEvents
     {
-        public static UnityEvent StartGame = new UnityEvent();
+        public static readonly UnityEvent<Enums.GameState> StartGame = new UnityEvent();
     }
+    
+    
+    public class UnityEvent : UnityEvent<Enums.GameState> { }
 }
