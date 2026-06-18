d32809d28e76e45ee0dc3df3af01d6c31e7e61f3
niflthaena <niflthaena@gmail.com>
2026-06-18 10:34:42 -0700

    Add placeholder store UI, split from main scene for ease of integration

--- Assets/_Code/Phases/StorePhase.cs
+++ Assets/_Code/Phases/StorePhase.cs
@@ -4,6 +4,7 @@ namespace LSG.Phases
 {
     public class StorePhase : Phase
     {
-        
+        [SerializeField] private PlayerEconomy playerEconomy;
+        [SerializeField] private GameObject Container;
     }
 }
