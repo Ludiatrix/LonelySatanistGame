6ddacf45acf8734c6fe5fb97a4127857711cf08c
ludiatrix <riza@nysom.com>
2026-06-18 13:58:30 -0700

    - Add the DataManager.cs

---
+++ Assets/_Code/Core/DataManager.cs
@@ -0,0 +1,33 @@
+using System;
+using LSG.ScriptableObjects;
+using UnityEngine;
+
+namespace LSG.Core
+{
+    /// <summary>
+    /// Singleton Instance Container for holding
+    /// Scriptable Objects for different data containers. 
+    /// </summary>
+    public class DataManager : MonoBehaviour
+    {
+        public static DataManager Instance { get; private set; }
+        
+        public PlayerEconomy PlayerEconomySource;
+        public MilestoneData MilestoneDataSource;
+        public PlayerDeck PlayerDeckSource;
+        public DemonDatingPool DemonDatingPoolSource;
+        
+        // Say the line, Bart.
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
