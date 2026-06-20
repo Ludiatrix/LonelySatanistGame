1ccf381aa72e410cec2c07064d73230c00ad763c
niflthaena <niflthaena@gmail.com>
2026-06-20 12:57:33 -0700

    Update Sanity display; candle burns down and flame tracks candle.

--- Assets/_Code/UI/SanityCanvasController.cs
+++ Assets/_Code/UI/SanityCanvasController.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using LSG.Core;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace LSG.UI
 {
@@ -12,10 +14,20 @@ namespace LSG.UI
         [SerializeField] private TMP_Text sanityText;
         [SerializeField] private GameObject sanityCandleFlame;
         [SerializeField] private AnimationCurve sanityFlameSize;
+        [SerializeField] private AnimationCurve sanityFlameOffset;
+
+        [SerializeField] private Image candleImage;
+
+        [Tooltip("Indexed by sanity amount. Start with lowest, end with highest")]
+        [SerializeField] private List<Sprite> candleSprites;
+
+        private float initialFlameOffset;
 
         private void OnEnable()
         {
             GameEvents.PageRead?.AddListener(OnPageRead);
+            initialFlameOffset = sanityCandleFlame.transform.localPosition.y;
+            ShowResults(20);
         }
 
         private void OnDisable()
@@ -25,9 +37,23 @@ namespace LSG.UI
 
         private void OnPageRead()
         {
-            sanityText.text = $"Sanity: {DataManager.Instance.PlayerEconomySource.Sanity.ToString()}";
-            float scale = sanityFlameSize.Evaluate(DataManager.Instance.PlayerEconomySource.Sanity);
+            ShowResults(DataManager.Instance.PlayerEconomySource.Sanity);
+        }
+
+        private void ShowResults(int sanity)
+        {
+            sanityText.text = $"Sanity: {sanity}";
+
+            float scale = sanityFlameSize.Evaluate(sanity);
             sanityCandleFlame.transform.localScale = new Vector3(scale, scale);
+
+            Vector3 flamePosition = sanityCandleFlame.transform.localPosition;
+            flamePosition.y = initialFlameOffset + sanityFlameOffset.Evaluate(sanity);
+            sanityCandleFlame.transform.localPosition = flamePosition;
+
+            // Set minimum range so sprites don't index oob
+            if (sanity < 1) sanity = 1;
+            candleImage.sprite = candleSprites[sanity - 1];
         }
     }
 }
