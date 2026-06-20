4c948c7198f34154a1c4d97a11a4822828df7250
niflthaena <niflthaena@gmail.com>
2026-06-20 14:49:16 -0700

    Replace rizz meter with flowerpot

--- Assets/_Code/UI/RizzCanvasController.cs
+++ Assets/_Code/UI/RizzCanvasController.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using LSG.Core;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace LSG.UI
 {
@@ -10,6 +12,8 @@ namespace LSG.UI
     public class RizzCanvasController : MonoBehaviour
     {
         [SerializeField] private TMP_Text rizzText;
+
+        [SerializeField] private List<GameObject> flowers;
         
         private void OnEnable()
         {
@@ -23,7 +27,14 @@ namespace LSG.UI
 
         private void OnPageRead()
         {
-            rizzText.text = $"Rizz: {DataManager.Instance.PlayerEconomySource.Rizz.ToString()}";
+            int rizz = DataManager.Instance.PlayerEconomySource.Rizz;
+            rizzText.text = $"Rizz: {rizz}";
+            for (var i = 0; i < 8; i++)
+            {
+                // I know we can bake the conditional into the call, but that's often harder to read
+                if (i >= rizz) flowers[i].SetActive(false);
+                else flowers[i].SetActive(true);
+            }
         }
     }
 }
