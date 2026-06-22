c9be68e52efa0e518362423f83999140c63fa4a8
chloe <cwoolwine@gmail.com>
2026-06-21 22:44:28 -0700

    fortran and hoa

--- Assets/_Code/Effects/DemonEffects/AdmiralFortranEffects.cs
+++ Assets/_Code/Effects/DemonEffects/AdmiralFortranEffects.cs
@@ -2,13 +2,15 @@ using System;
 using LSG.Classes;
 using LSG.Core;
 using LSG.Interfaces;
+using LSG.ScriptableObjects;
 using UnityEngine;
 
 namespace LSG.Effects
 {
     /// <summary>
     /// Please update if changed
-    /// Boon: TODO: Write a boon for the Admiral!
+    /// Boon: The next time you get 8 white points, the last white card is shuffled back
+	/// into the deck
     /// Bane: Lose 1 Tape (if the player has a tape banked)
     /// </summary>
     public class AdmiralFortranEffects : MonoBehaviour, IEffectable
@@ -16,23 +18,57 @@ namespace LSG.Effects
         [SerializeField] private ModifierPayload boonPayload;
         [SerializeField] private ModifierPayload banePayload;
 
+        private bool _hasRedo = true;
+
+        private void OnEnable()
+        {
+            GameEvents.CardTaken?.AddListener(OnCardTaken);
+        }
+
+        private void OnDisable()
+        {
+            GameEvents.CardTaken?.RemoveListener(OnCardTaken);
+        }
+
         private void Start()
         {
-            boonPayload = new ModifierPayload
-            {
-                Tape = 1
-            };
+            boonPayload = new ModifierPayload();
             
             banePayload = new ModifierPayload
             {
-                Sanity = -2
+                Tape = -1
             };
         }
 
-        public void ApplyBoon()
+        private void OnCardTaken(CardData takenCard)
         {
-            EconomyEvents.SendPayload?.Invoke(boonPayload);
+            if (!_hasRedo) return;
+            if (takenCard.Suit != Enums.Suit.White) return;
+
+            var economy = DataManager.Instance.PlayerEconomySource;
+            if (economy.WhiteSuitPoints < 7) return;
+
+            _hasRedo = false;
+	
+            if (DataManager.Instance.PlayerDeckSource.playedCards.Contains(takenCard))
+            {
+                DataManager.Instance.PlayerDeckSource.playedCards.Remove(takenCard);
+            }
+
+            DataManager.Instance.PlayerDeckSource.playerDeck.Add(takenCard);
+            DataManager.Instance.PlayerDeckSource.Shuffle();
+
+            if (economy.WhiteSuitPoints > 0)
+            {
+                economy.WhiteSuitPoints--;
+            }	
+            Debug.Log("[AdmiralFortranEffects] Redo triggered: white card returned to deck.");
         }
+
+        public void ApplyBoon()
+		{
+			// handled in OnCardTaken
+		}
 
         public void ApplyBane()
         {
--- Assets/_Code/Effects/DemonEffects/HoaEffects.cs
+++ Assets/_Code/Effects/DemonEffects/HoaEffects.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using LSG.Classes;
 using LSG.Core;
 using LSG.Interfaces;
@@ -7,36 +8,49 @@ namespace LSG.Effects
 {
     /// <summary>
     /// Please update if changed
-    /// Boon: TODO: Write a boon for Hoa
+    /// Boon: All [orange suit] pages are +1 power for the rest of this game.
     /// Bane: One of your non-white pages is randomly removed and cannot be purchased again this game.
     /// </summary>
     public class HoaEffects : MonoBehaviour, IEffectable
     {
         [SerializeField] private ModifierPayload boonPayload;
         [SerializeField] private ModifierPayload banePayload;
+        private readonly HashSet<CardData> _boostedOrangeCards = new HashSet<CardData>();
+
+        private void OnEnable()
+        {
+            GameEvents.CardTaken?.AddListener(OnCardTaken);
+        }
+
+        private void OnDisable()
+        {
+            GameEvents.CardTaken?.RemoveListener(OnCardTaken);
+        }
 
         private void Start()
         {
-            boonPayload = new ModifierPayload
-            {
-                Power = 2
-            };
-            
-            banePayload = new ModifierPayload
-            {
-                Sanity = -1
-            };
+            boonPayload = new ModifierPayload();
+            banePayload = new ModifierPayload();
+        }
+
+        private void OnCardTaken(CardData takenCard)
+        {
+            if (takenCard == null) return;
+            if (takenCard.Suit != Enums.Suit.Orange) return;
+            if (_boostedOrangeCards.Contains(takenCard)) return;
+
+            takenCard.PageModifier.Power += 1;
+            _boostedOrangeCards.Add(takenCard);
         }
 
         public void ApplyBoon()
         {
-            EconomyEvents.SendPayload?.Invoke(boonPayload);
+            // card event listener
         }
 
         public void ApplyBane()
         {
-            
-            EconomyEvents.SendPayload?.Invoke(banePayload);
+            CardEvents.RemoveRandomCard?.Invoke(Enums.Suit.White, false);
         }
     }
 }
