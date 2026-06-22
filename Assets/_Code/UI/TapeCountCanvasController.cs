1fd3edf143b5d5db34e42aded1d966b3b18294fa
ludiatrix <riza@nysom.com>
2026-06-21 22:58:42 -0700

    Fix positioning issues with the Pick a Card canvas

--- Assets/_Code/UI/TapeCountCanvasController.cs
+++ Assets/_Code/UI/TapeCountCanvasController.cs
@@ -13,28 +13,7 @@ namespace LSG.UI
     {
         [SerializeField] private TMP_Text tapeCountText;
 
-        private void OnEnable()
-        {
-            GameEvents.PageRead?.AddListener(OnPageRead);
-            GameEvents.CardAdded?.AddListener(OnUpdate);
-            GameEvents.CardTaken?.AddListener(OnUpdate);
-            CardEvents.BuyCardSuccessResponse?.AddListener(OnUpdate);
-        }
-
-        private void OnDisable()
-        {
-            GameEvents.PageRead?.RemoveListener(OnPageRead);
-            GameEvents.CardAdded?.RemoveListener(OnUpdate);
-            GameEvents.CardTaken?.RemoveListener(OnUpdate);
-            CardEvents.BuyCardSuccessResponse?.RemoveListener(OnUpdate);
-        }
-
-        private void OnPageRead()
-        {
-            tapeCountText.text = $"{DataManager.Instance.PlayerEconomySource.Tape.ToString()}";
-        }
-
-        private void OnUpdate(CardData data = null)
+        private void LateUpdate()
         {
             tapeCountText.text = $"{DataManager.Instance.PlayerEconomySource.Tape.ToString()}";
         }
